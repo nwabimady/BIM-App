27f5b30a45a9cb59eb474e4094674aa1086e5853
nwabimady <nwabimady@gmail.com>
2023-09-06 21:56:57 +0100

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MapViewer } from "./components/map-viewer";
+import { LoginForm } from "./components/login-form";
+import { BuildingViewer } from "./components/building-viewer";
 
 function App() {
-  return <h1> Hello world! </h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/building" element={<BuildingViewer />} />
+        <Route path="/map" element={<MapViewer />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/" element={<LoginForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
