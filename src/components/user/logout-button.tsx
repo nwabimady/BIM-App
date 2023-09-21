8e9fc184660d006dc6fc49643608653805188522
nwabimady <nwabimady@gmail.com>
2023-09-21 22:36:17 +0100

    Add material UI

--- src/components/user/logout-button.tsx
+++ src/components/user/logout-button.tsx
@@ -1,10 +1,18 @@
 import { signOut, getAuth } from "firebase/auth";
 import { FC } from "react";
+import Button from "@mui/material/Button";
 
 export const LogOutButton: FC = () => {
   const auth = getAuth();
   const onLogoutClick = () => {
     signOut(auth);
   };
-  return <button onClick={onLogoutClick}>Logout</button>;
+
+  return (
+    <>
+      <Button onClick={onLogoutClick} variant="contained">
+        Log out
+      </Button>
+    </>
+  );
 };
--- src/middleware/authenticator.tsx
+++ src/middleware/authenticator.tsx
@@ -1,6 +1,7 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { FC, useEffect } from "react";
 import { useAppContext } from "./context-provider";
+import { Action } from "./actions";
 
 let authInitialised = false;
 
@@ -11,7 +12,7 @@ export const Authenticator: FC = () => {
   const listenToAuthChanges = () => {
     onAuthStateChanged(auth, (foundUser) => {
       const user = foundUser ? { ...foundUser } : null;
-      dispatch({ type: "UPDATE_USER", payload: user });
+      dispatch({ type: "UPDATE_USER", payload: user } as Action); // type assertion
     });
   };
 
